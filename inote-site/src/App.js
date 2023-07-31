2a7023419a84c0b87ecec3bb9fc4b7411e1e0ed5
oluwatobisa <oluwatobisamuel37@gmail.com>
2023-08-01 00:52:00 +0100

    first commit

---
+++ inote-site/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className=" flex mt-6 justify-center">
+      <div className="flex">
+        <h1>iNOTE</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
